a8d19e119cfcc0bf6691df17d57598f8d07df1ff
Kevin Lou <58053969+klou23@users.noreply.github.com>
2024-04-26 18:52:54 -0400

    dropdown form

--- frontend/src/Components/Field.jsx
+++ frontend/src/Components/Field.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {TextForm} from "./TextForm";
+import {TextForm} from "./FieldComponents/TextForm";
 import {FieldTitle} from "./FieldComponents/FieldTitle";
+import {DropdownForm} from "./FieldComponents/DropdownForm";
 
-export function Field({fields, data, setData, index, recording}) {
+export function Field({fields, data, setData, index, recording, serverAddr}) {
     
     function getField() {
         //console.log(JSON.stringify(fields[index]))
@@ -11,7 +12,7 @@ export function Field({fields, data, setData, index, recording}) {
         } else if (fields[index].automatic) {
             return (<></>)
         } else if(fields[index].dropdown) {
-            return (<></>)
+            return (<DropdownForm fields={fields} data={data} setData={setData} index={index} recording={recording} serverAddr={serverAddr}/>)
         } else {
             return (<TextForm fields={fields} data={data} setData={setData} index={index} recording={recording}/>)
         }
