840dd0e10695c2598416a0f796098c714c3e4418
Christine <132790780+aesteri@users.noreply.github.com>
2024-07-15 15:18:51 -0400

    Update runner.py

--- py_data_acq/runner.py
+++ py_data_acq/runner.py
@@ -135,7 +135,6 @@ async def continuous_video_receiver(queue, q2):
         compressed_image = QueueData(compressed_image.DESCRIPTOR.name, compressed_image)
         await queue.put(compressed_image)
         await q2.put(compressed_image)
-        await asyncio.sleep(0)
 
 
 async def continuous_can_receiver(
@@ -165,7 +164,6 @@ async def continuous_can_receiver(
             data = QueueData(msg.DESCRIPTOR.name, msg)
             await queue.put(data)
             await q2.put(data)
-            await asyncio.sleep(0)
 
 
         except Exception as e:
