e2c7f388e7043fd29a65d21f15bc8ebdcf3a4655
Christine <132790780+aesteri@users.noreply.github.com>
2024-07-15 14:45:40 -0400

    Update runner.py

--- py_data_acq/runner.py
+++ py_data_acq/runner.py
@@ -135,7 +135,7 @@ async def continuous_video_receiver(queue, q2):
         compressed_image = QueueData(compressed_image.DESCRIPTOR.name, compressed_image)
         await queue.put(compressed_image)
         await q2.put(compressed_image)
-        #await asyncio.sleep(0)
+        await asyncio.sleep(0)
 
 
 async def continuous_can_receiver(
@@ -165,6 +165,9 @@ async def continuous_can_receiver(
             data = QueueData(msg.DESCRIPTOR.name, msg)
             await queue.put(data)
             await q2.put(data)
+            await asyncio.sleep(0)
+
+
         except Exception as e:
             # print(id)
             # print(e)
@@ -265,15 +268,12 @@ async def run(logger):
         init_filename=mcap_writer.actual_path
     )
     receiver_task = asyncio.create_task(
-        asyncio.gather(
-            continuous_can_receiver(db, msg_pb_classes, queue, queue2, bus),
-            # continuous_aero_receiver(queue, queue2),
-            continuous_video_receiver(queue, queue2)
-        )
+        continuous_can_receiver(db, msg_pb_classes, queue, queue2, bus)
     )
 
     #testing these two tasks
-    
+    #aero_receiver_task = asyncio.create_task(continuous_aero_receiver(queue, queue2))
+    video_receiver_task = asyncio.create_task(continuous_video_receiver(queue, queue2))
 
     fx_task = asyncio.create_task(fxglv_websocket_consume_data(queue, fx_s))
     mcap_task = asyncio.create_task(write_data_to_mcap(mcap_writer_cmd_queue, mcap_writer_status_queue, queue2, mcap_writer, init_writing_on_start))
@@ -284,7 +284,7 @@ async def run(logger):
     # and schema in the foxglove websocket server.
 
 #edited tasks
-    await asyncio.gather(receiver_task, fx_task, mcap_task, srv_task)
+    await asyncio.gather(receiver_task, video_receiver_task, fx_task, mcap_task, srv_task)
 
 if __name__ == "__main__":
     logging.basicConfig()
