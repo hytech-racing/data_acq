ecfb4a776f55325d284b820c14f7653d951c130a
Christine <132790780+aesteri@users.noreply.github.com>
2024-07-18 04:02:20 -0400

    Update runner.py

--- py_data_acq/runner.py
+++ py_data_acq/runner.py
@@ -141,7 +141,6 @@ async def continuous_video_receiver(queue, q2):
             logger.info("/dev/video1 opened successfully")
     else:
         logger.info("/dev/video0 opened successfully")
-    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
     while True:
